310b216cfd0176ceea03f777c6aaf1feb367ad19
Mario Reder <mario.reder@pm.me>
2024-05-27 14:59:58 +0200

    prevent spawning infinite threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use chrono::prelude::*;
-use futures::future::join_all;
 use near_jsonrpc_client::{methods, JsonRpcClient};
 use near_jsonrpc_primitives::types::chunks::ChunkReference;
 use near_primitives::{
@@ -8,8 +7,8 @@ use near_primitives::{
 };
 use near_workspaces::AccountId;
 use parking_lot::RwLock;
-use std::{collections::HashSet, sync::Arc};
-use tokio::fs;
+use std::{collections::HashSet, sync::Arc, time::Duration};
+use tokio::{fs, time::sleep};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -43,42 +42,43 @@ async fn main() -> anyhow::Result<()> {
     } else {
         panic!();
     };
-    let tasks = ((block_height - block_count + 1)..block_height)
-        .rev()
-        .map(|height| {
-            let rpc_client = rpc_client.clone();
-            let all_signers = all_signers.clone();
-            tokio::spawn(async move {
-                if let Ok(block) = rpc_client
-                    .call(methods::block::RpcBlockRequest {
-                        block_reference: BlockReference::BlockId(BlockId::Height(height)),
-                    })
-                    .await
-                {
-                    if (height - block_height + block_count) % 10_000 == 0 {
-                        println!("remaining: {}", height - block_height + block_count);
-                    }
-                    handle_block(block, &rpc_client, all_signers.clone()).await;
-                }
+
+    let cpus = num_cpus::get();
+    let lock = Arc::new(RwLock::new(0));
+    for height in ((block_height - block_count + 1)..block_height).rev() {
+        loop {
+            if *lock.read() + 1 == cpus * 2 {
+                sleep(Duration::from_millis(100)).await;
+            } else {
+                break;
+            }
+        }
+        {
+            let mut lock = lock.write();
+            *lock += 1;
+        }
+        let rpc_client = rpc_client.clone();
+        let all_signers = all_signers.clone();
+        let lock = lock.clone();
+        tokio::spawn(async move {
+            let block = tryhard::retry_fn(|| {
+                rpc_client.call(methods::block::RpcBlockRequest {
+                    block_reference: BlockReference::BlockId(BlockId::Height(height)),
+                })
             })
-        })
-        .collect::<Vec<_>>();
-    join_all(tasks).await;
-    // let mut i = 0;
-    // for block_height in (block_height - block_count + 1)..block_height {
-    //     if let Ok(block) = rpc_client
-    //         .call(methods::block::RpcBlockRequest {
-    //             block_reference: BlockReference::BlockId(BlockId::Height(block_height)),
-    //         })
-    //         .await
-    //     {
-    //         handle_block(block, &rpc_client, all_signers.clone()).await;
-    //     }
-    //     i += 1;
-    //     if i % 100 == 0 {
-    //         println!("remaining: {}", block_count - i);
-    //     }
-    // }
+            .retries(20)
+            .fixed_backoff(Duration::from_millis(100))
+            .await
+            .unwrap();
+
+            handle_block(block, &rpc_client, all_signers.clone()).await;
+            if (height - block_height + block_count) % 100 == 0 {
+                println!("remaining: {}", height - block_height + block_count);
+            }
+            let mut lock = lock.write();
+            *lock -= 1;
+        });
+    }
 
     dbg!(all_signers.read().len());
     let naive = NaiveDateTime::from_timestamp((first_block_timestamp / 1_000_000_000) as i64, 0);
@@ -87,15 +87,15 @@ async fn main() -> anyhow::Result<()> {
 
     dbg!();
     let all_signers = Arc::try_unwrap(all_signers).unwrap().into_inner();
-    // fs::write(
-    //     "account_ids",
-    //     all_signers
-    //         .iter()
-    //         .map(|account_id| account_id.as_str())
-    //         .collect::<Vec<_>>()
-    //         .join("\n"),
-    // )
-    // .await?;
+    fs::write(
+        "account_ids",
+        all_signers
+            .iter()
+            .map(|account_id| account_id.as_str())
+            .collect::<Vec<_>>()
+            .join("\n"),
+    )
+    .await?;
 
     anyhow::Ok(())
 }
@@ -107,30 +107,32 @@ async fn handle_block(
 ) {
     let near: AccountId = "near".parse().unwrap();
     for chunk in block.chunks {
-        if let Ok(chunk) = rpc_client
-            .call(methods::chunk::RpcChunkRequest {
+        let chunk = tryhard::retry_fn(|| {
+            rpc_client.call(methods::chunk::RpcChunkRequest {
                 chunk_reference: ChunkReference::ChunkHash {
                     chunk_id: chunk.chunk_hash,
                 },
             })
-            .await
-        {
-            for transaction in chunk.transactions {
-                if !transaction.signer_id.as_str().ends_with(".near") {
-                    continue;
-                }
-                if !transaction.signer_id.is_sub_account_of(&near) {
-                    continue;
-                }
-                if transaction.signer_id.as_str().contains("relayer") {
-                    continue;
-                }
-                if transaction.signer_id.as_str().contains("oracle") {
-                    continue;
-                }
-                if !all_signers.read().contains(&transaction.signer_id) {
-                    all_signers.write().insert(transaction.signer_id);
-                }
+        })
+        .retries(20)
+        .fixed_backoff(Duration::from_millis(100))
+        .await
+        .unwrap();
+        for transaction in chunk.transactions {
+            if !transaction.signer_id.as_str().ends_with(".near") {
+                continue;
+            }
+            if !transaction.signer_id.is_sub_account_of(&near) {
+                continue;
+            }
+            if transaction.signer_id.as_str().contains("relayer") {
+                continue;
+            }
+            if transaction.signer_id.as_str().contains("oracle") {
+                continue;
+            }
+            if !all_signers.read().contains(&transaction.signer_id) {
+                all_signers.write().insert(transaction.signer_id);
             }
         }
     }
