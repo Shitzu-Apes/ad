346dbba3a79732fb1219dd01afeeb23539ce00ad
Mario Reder <mario.reder@pm.me>
2024-05-28 13:39:55 +0200

    check staking

--- crates/accounts/src/main.rs
+++ crates/accounts/src/main.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use dashmap::DashMap;
 use near_jsonrpc_client::{methods, JsonRpcClient};
 use near_jsonrpc_primitives::types::chunks::ChunkReference;
 use near_primitives::{
@@ -7,6 +8,8 @@ use near_primitives::{
 };
 use near_workspaces::AccountId;
 use parking_lot::RwLock;
+use reqwest::Client;
+use serde::Deserialize;
 use std::{collections::HashSet, sync::Arc, time::Duration};
 use tokio::{fs, time::sleep};
 
@@ -15,8 +18,9 @@ async fn main() -> anyhow::Result<()> {
     let rpc_client = JsonRpcClient::connect("https://beta.rpc.mainnet.near.org");
     // let rpc_client = JsonRpcClient::connect("http://localhost");
 
-    let block_count = 500_000;
+    let block_count = 2_000_000;
     let all_signers = Arc::new(RwLock::new(HashSet::new()));
+    let stakers = Arc::new(DashMap::new());
 
     let block_height = if let Ok(block) = rpc_client
         .call(methods::block::RpcBlockRequest {
@@ -25,7 +29,7 @@ async fn main() -> anyhow::Result<()> {
         .await
     {
         let block_height = block.header.height;
-        handle_block(block, &rpc_client, all_signers.clone()).await;
+        handle_block(block, stakers.clone(), &rpc_client, all_signers.clone()).await;
         block_height
     } else {
         panic!();
@@ -37,7 +41,7 @@ async fn main() -> anyhow::Result<()> {
         .await
     {
         let block_timestamp = block.header.timestamp;
-        handle_block(block, &rpc_client, all_signers.clone()).await;
+        handle_block(block, stakers.clone(), &rpc_client, all_signers.clone()).await;
         block_timestamp
     } else {
         panic!();
@@ -47,7 +51,7 @@ async fn main() -> anyhow::Result<()> {
     let lock = Arc::new(RwLock::new(0));
     for height in ((block_height - block_count + 1)..block_height).rev() {
         loop {
-            if *lock.read() + 1 == cpus * 4 {
+            if *lock.read() + 1 == cpus * 3 {
                 sleep(Duration::from_millis(100)).await;
             } else {
                 break;
@@ -59,6 +63,7 @@ async fn main() -> anyhow::Result<()> {
         }
         let rpc_client = rpc_client.clone();
         let all_signers = all_signers.clone();
+        let stakers = stakers.clone();
         let lock = lock.clone();
         tokio::spawn(async move {
             if let Ok(block) = tryhard::retry_fn(|| {
@@ -70,23 +75,28 @@ async fn main() -> anyhow::Result<()> {
             .fixed_backoff(Duration::from_millis(100))
             .await
             {
-                handle_block(block, &rpc_client, all_signers.clone()).await;
+                handle_block(block, stakers, &rpc_client, all_signers.clone()).await;
             }
 
             if (height + block_count - block_height) % 100 == 0 {
-                println!("remaining: {}", height + block_count - block_height);
+                println!(
+                    "remaining: {}; accounts: {}",
+                    height + block_count - block_height,
+                    all_signers.read().len()
+                );
             }
             let mut lock = lock.write();
             *lock -= 1;
         });
     }
 
-    dbg!(all_signers.read().len());
     let naive = NaiveDateTime::from_timestamp((first_block_timestamp / 1_000_000_000) as i64, 0);
     let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
     dbg!(datetime);
 
-    dbg!();
+    while Arc::strong_count(&all_signers) > 1 {
+        sleep(Duration::from_millis(100)).await;
+    }
     let all_signers = Arc::try_unwrap(all_signers).unwrap().into_inner();
     fs::write(
         "account_ids",
@@ -103,9 +113,11 @@ async fn main() -> anyhow::Result<()> {
 
 async fn handle_block(
     block: BlockView,
+    stakers: Arc<DashMap<AccountId, bool>>,
     rpc_client: &JsonRpcClient,
     all_signers: Arc<RwLock<HashSet<AccountId>>>,
 ) {
+    let client = Client::new();
     let near: AccountId = "near".parse().unwrap();
     for chunk in block.chunks {
         if let Ok(chunk) = tryhard::retry_fn(|| {
@@ -120,9 +132,6 @@ async fn handle_block(
         .await
         {
             for transaction in chunk.transactions {
-                if !transaction.signer_id.as_str().ends_with(".near") {
-                    continue;
-                }
                 if !transaction.signer_id.is_sub_account_of(&near) {
                     continue;
                 }
@@ -132,10 +141,60 @@ async fn handle_block(
                 if transaction.signer_id.as_str().contains("oracle") {
                     continue;
                 }
+                let is_staking =
+                    fetch_staking(&transaction.signer_id, stakers.clone(), &client).await;
+                if !is_staking {
+                    continue;
+                }
                 if !all_signers.read().contains(&transaction.signer_id) {
                     all_signers.write().insert(transaction.signer_id);
                 }
             }
         }
     }
+}
+
+async fn fetch_staking(
+    account_id: &AccountId,
+    stakers: Arc<DashMap<AccountId, bool>>,
+    client: &Client,
+) -> bool {
+    let is_staking = stakers.get_mut(account_id);
+    match is_staking {
+        Some(is_staking) => *is_staking,
+        None => {
+            let check_is_staking = if let Ok(res) = tryhard::retry_fn(|| {
+                client
+                    .get(format!(
+                        "https://api.fastnear.com/v1/account/{}/staking",
+                        account_id
+                    ))
+                    .send()
+            })
+            .retries(20)
+            .fixed_backoff(Duration::from_millis(100))
+            .await
+            {
+                if let Ok(staking) = res.json::<StakingResponse>().await {
+                    !staking.pools.is_empty()
+                } else {
+                    false
+                }
+            } else {
+                false
+            };
+            stakers.insert(account_id.clone(), check_is_staking);
+            check_is_staking
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct StakingResponse {
+    pub pools: Vec<StakedPool>,
+}
+
+#[derive(Deserialize)]
+pub struct StakedPool {
+    pub pool_id: String,
 }
